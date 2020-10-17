5d5b36ca7c028f83a2c233f8b6a189c45d7891c1
crashdemons <crashenator@gmail.com>
2020-10-17 16:55:31 -0500

    demo custom heads from snowballs with loretext

--- src/main/java/com/github/crashdemons/aprilfools/Plugin.java
+++ src/main/java/com/github/crashdemons/aprilfools/Plugin.java
@@ -5,13 +5,22 @@
  */
 package com.github.crashdemons.aprilfools;
 
+import com.github.crashdemons.playerheads.compatibility.Compatibility;
+import com.github.crashdemons.playerheads.compatibility.CompatibleProfile;
+import com.github.crashdemons.playerheads.compatibility.CompatibleSkullMaterial;
+import java.util.List;
+import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.ThrowableProjectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -19,19 +28,66 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author crashdemons (crashenator at gmail.com)
  */
 public class Plugin extends JavaPlugin implements Listener {
+    
+    private ItemStack stack = null;
  
     @Override
     public void onEnable(){
-        this.getServer().getPluginManager().registerEvents(this, this);
+        
+        //PlayerHeadsAPI phapi = PlayerHeads.getApiInstance();
+        stack = Compatibility.getProvider().getCompatibleHeadItem(CompatibleSkullMaterial.PLAYER, 1);
+        stack = Compatibility.getProvider().applyDefaultItemMeta(stack, false);
+        
+        
+        CompatibleProfile profile = Compatibility.getProvider().createCompatibleProfile(null, UUID.fromString("51d45a7a-34cf-425b-86ab-41f809a1de38"), "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjBiZDIwMTI4YzcxMjEwNTA1ZDgwNjJhNTFhZTJhYmUwY2MzZmNhNTAxMDdmODlmMTJkM2E4ZDZkY2ZkYWVhMSJ9fX0=");
+        ItemMeta meta = stack.hasItemMeta() ? stack.getItemMeta() : Bukkit.getItemFactory().getItemMeta(Material.PLAYER_HEAD);
+        if(meta==null) throw new IllegalStateException("Player Head has no ItemMeta");
+        Compatibility.getProvider().setCompatibleProfile(meta, profile);
+        stack.setItemMeta(meta);
+        
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
     
     
     @EventHandler(ignoreCancelled=true)
     public void onProjectile(ProjectileLaunchEvent event){
+        if(stack==null){
+            getLogger().warning("stack is null");
+            return;
+        }
         Projectile proj = event.getEntity();
+        if(proj instanceof EnderPearl) return;
         if(proj instanceof ThrowableProjectile){
+            //getLogger().info("setting projectile image");
             ThrowableProjectile tproj = (ThrowableProjectile) proj;
-            tproj.setItem(new ItemStack(Material.DIAMOND));
+            ItemStack stackold = tproj.getItem();
+            
+            boolean isCustomSnowball = false;
+            
+            if(stackold.hasItemMeta()){
+                ItemMeta meta = stackold.getItemMeta();
+                if(meta.hasLore()){
+                    List<String> lore = meta.getLore();
+                    
+                    for(String line : lore){
+                       if( ChatColor.stripColor(line).equals("halloween") ){
+                           isCustomSnowball = true;
+                           break;
+                       }
+                    }
+                    
+                }
+                
+            }
+            if(!isCustomSnowball) return;
+            
+            
+            
+            
+            tproj.setItem(stack);
+           
+            
+            
         }
     }
 }
